21ee295fa0e426f8c4ee0b46e278ce8595d28b4c
Brooks, Mark <mark.brooks@evotec.com>
2026-04-03 15:47:49 -0400

    initial commit

---
+++ _openfold/np/residue_constants.py
@@ -0,0 +1,6 @@
+restype_num = 20
+
+restypes_with_x = [
+    "A","R","N","D","C","Q","E","G","H","I",
+    "L","K","M","F","P","S","T","W","Y","V","X"
+]
---
+++ _openfold/utils/loss.py
@@ -0,0 +1,7 @@
+import torch
+
+def compute_predicted_aligned_error(*args, **kwargs):
+    return {}
+
+def compute_tm(*args, **kwargs):
+    return torch.tensor(0.0)
